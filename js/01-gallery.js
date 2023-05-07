ea3f06864b77ca3c5ee08ef4026f8d6d95d28e4b
OksanaGreen <Springocsana@ukr.net>
2023-05-07 19:31:35 +0300

    Homework-7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,48 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-// const imageEl = document.querySelector(".gallery");
-// function createGalMarckup(items) {
-//   return items
-//     .map(
-//       ({ preview, original, description }) =>
-//         `<li class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </li> `
-//     )
-//     .join("");
-// }
-// const addteGalMarckup = createGalMarckup(galleryItems);
-// imageEl.innerHTML = addteGalMarckup;
-// imageEl.addEventListener("click", onGalleryItemsClick);
-// function onGalleryItemsClick(evt) {
-//   evt.preventDefault();
-//   const imgGalleryEl = evt.target.classList.contains("gallery__image");
-//   if (imgGalleryEl) {
-//     return;
-//   }
-// }
-// // const instance = basicLightbox.create();
-// // instance.show();
-// // import * as basicLightbox from 'basiclightbox'
 
-// // const instance = basicLightbox.create(`
-// //     <img src="assets/images/image.png" width="800" height="600">
-// // `)
-
-// // instance.show()
-// imageEl.insertAdjacentHTML("beforeend", createGalMarckup);
-// new SimpleLightbox(".gallery a", {
-//   captionType: "attr",
-//   captionDelay: 250,
-// });
 // Доступ до елемента
 const gallery = document.querySelector(".gallery");
 const markup = galleryItems.map(
@@ -54,23 +13,20 @@ const markup = galleryItems.map(
    </a>
 </li>`
 );
-// function onGalleryItemsClick(evt) {
-//   evt.preventDefault();
-//   const imgGalleryEl = evt.target.classList.contains("gallery__image");
-//   if (imgGalleryEl) {
-//     return;
-//   }
-// }
-// form.addEventListener("klick", (event) => {
-//   event.preventDefault();
+
 gallery.insertAdjacentHTML("beforeend", markup.join(""));
-new SimpleLightbox(".gallery a", {
-  captionType: "attr",
-  captionDelay: 250,
-});
+// new SimpleLightbox(".gallery a", {
+//   captionType: "attr",
+//   captionDelay: 250,
+// });
+const instance = galleryItems.create(`
+    <img class="gallery__image" src = '${preview}' alt='${description}' />
+ `);
 
-// const instance = galleryItems.create(`
-//    <img class="gallery__image" src = '${preview}' alt='${description}' />
-// `);
-
-// instance.show()
+gallery.addEventListener("clik", onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  instance.show();
+  instance.show(() => console.log("lightbox now visible"));
+  return;
+}
