c7e14249c609c072428f87b43a4381e97641a079
OksanaGreen <Springocsana@ukr.net>
2023-05-18 21:16:44 +0300

    corect

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+let original;
+let preview;
+let description;
 const gallery = document.querySelector(".gallery");
 const galleryMarcup = galleryItems
   .map(
@@ -16,15 +20,20 @@ const galleryMarcup = galleryItems
   )
   .join("");
 gallery.insertAdjacentHTML("beforeend", galleryMarcup);
-const instance = basicLightbox.create(`
-    <img src= ${evt.target.dataset.source}  width="800" height="600">
-`);
 
-instance.show();
-const lightbox = $(".gallery__image").simpleLightbox({
+// const instance = basicLightbox.create(`
+//     <img src= ${evt.target.dataset.source}  width="800" height="600">
+// `);
+
+// instance.show();
+var lightbox = new SimpleLightbox(".gallery__link", {
   captionsData: "alt",
-  captionDelay: 250,
+  captionDelay: "250",
 });
+// const lightbox = $(".gallery__image").simpleLightbox({
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
 // const lightbox = new SimpleLightbox(".gallery__image", {
 //   captionsData: "alt",
 //   captionDelay: 250,
